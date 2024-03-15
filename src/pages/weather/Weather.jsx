71ac3173d25d82c50c8ce588a439c9e10fc6e142
Aman Kumar Singh <amansingh844123@gmail.com>
2024-03-15 14:34:12 +0530

    Updated Weather Page

--- src/pages/weather/Weather.jsx
+++ src/pages/weather/Weather.jsx
@@ -1,7 +1,13 @@
 import './Weather.css';
 import React, { useState } from 'react';
 import { fetchWeather, fetchCountry } from './WeatherService';
-import { Container, TextField, Box, Button, Card, Typography } from '@mui/material';
+import { Container, TextField, Box, Button, Card, Typography, Divider } from '@mui/material';
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
+import WindPowerIcon from '@mui/icons-material/WindPower';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ElectricMeterIcon from '@mui/icons-material/ElectricMeter';
+import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
 
 export default function WeatherApp() {
     const [city, setCity] = useState('');
@@ -11,16 +17,31 @@ export default function WeatherApp() {
     const handleSearch = async () => {
         if (!city) return;
         const data = await fetchWeather(city);
-        setWeather(data.list[0]);
-        if (data.list[0].sys.country) {
+        if (data.list.length === 0) {
+            setWeather('No Data');
+        } else if (data.list[0].sys.country) {
+            setWeather(data.list[0]);
             const countryData = await fetchCountry(data.list[0].sys.country);
             setCountry(countryData);
+        } else {
+            setWeather('No Data');
         }
     };
 
+    function capitalizeWord(word) {
+        return word
+            .split(' ')
+            .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+            .join(' ');
+    }
+
+    function fahrenheitToCelsius(fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9).toFixed(2);
+    }
+
     return (
         <div className='weather-main'>
-            <Container maxWidth="sm" sx={{ mt: 5 }}>
+            <Container maxWidth="sm">
                 <Card sx={{ p: 2 }}>
                     <Typography variant="h5" gutterBottom>
                         Weather Finder
@@ -37,16 +58,42 @@ export default function WeatherApp() {
                     </Button>
                     {weather && country && (
                         <Box>
-                            <Typography variant="h6">{`${weather.name}, ${country.name.official} ${country.flag}`}</Typography>
-                            <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt={weather.weather[0].description} />
-                            <Typography variant="body1">Description: {weather.weather[0].description}</Typography>
-                            <Typography variant="body1">Temperature: {weather.main.temp}°C</Typography>
-                            <Typography variant="body1">Feels Like: {weather.main.feels_like}°C</Typography>
-                            <Typography variant="body1">Min Temp: {weather.main.temp_min}°C, Max Temp: {weather.main.temp_max}°C</Typography>
-                            <Typography variant="body1">Pressure: {weather.main.pressure} hPa</Typography>
-                            <Typography variant="body1">Humidity: {weather.main.humidity}%</Typography>
-                            <Typography variant="body1">Wind Speed: {weather.wind.speed} m/s</Typography>
+                            <div className='weather-result'>
+                                <Typography variant="h6" fontWeight={700}>{`${weather.name}, ${country.name.official} ${country.flag}`}</Typography>
+                                <div className='weather-icons'>
+                                    <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt={weather.weather[0].description} />
+                                    <div>
+                                        <Typography variant="h4" fontWeight={700}>{fahrenheitToCelsius(weather.main.temp)}°C</Typography>
+                                        <Typography variant="subtitle1">{capitalizeWord(weather.weather[0].description)}</Typography>
+                                    </div>
+                                </div>
+                                <div className="weather-temp">
+                                    <DeviceThermostatIcon color='primary' />
+                                    <Typography variant="subtitle1">Feels Like: {weather.main.feels_like}°C</Typography>
+                                </div>
+                                <div className="weather-temp">
+                                    <ArrowDownwardIcon color='primary' />
+                                    <Typography variant="subtitle1">{weather.main.temp_min}°C</Typography>
+                                    <ArrowUpwardIcon color='primary' />
+                                    <Typography variant="subtitle1">{weather.main.temp_max}°C</Typography>
+                                </div>
+                                <div className="weather-temp">
+                                    <WaterDropIcon color='primary' />
+                                    <Typography variant="subtitle1">Humidity: {weather.main.humidity}%</Typography>
+                                </div>
+                                <div className="weather-temp">
+                                    <WindPowerIcon color='primary' />
+                                    <Typography variant="subtitle1">Wind Speed: {weather.wind.speed} m/s</Typography>
+                                </div>
+                                <div className="weather-temp">
+                                    <ElectricMeterIcon color='primary' />
+                                    <Typography variant="subtitle1">Pressure: {weather.main.pressure} hPa</Typography>
+                                </div>
+                            </div>
                         </Box>
+                    )}
+                    {weather == 'No Data' && (
+                        <Typography variant="h6" fontWeight={700}>City Not Found!!</Typography>
                     )}
                 </Card>
             </Container>
