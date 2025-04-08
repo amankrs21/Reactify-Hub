a1ccec356b94e936933a053e6766513b3a3dcef7
Aman Singh <amankrs212@gmail.com>
2025-04-09 01:10:49 +0530

    refactor: reorganize imports and clean up route definitions in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from './pages/home/Home';
-import PasswordVal from './pages/password/PasswordVal';
-import Navbar from './pages/navbar/Navbar';
-import Weather from './pages/weather/Weather';
 import Bmi from './pages/bmi/Bmi';
-import Calculator from './pages/calculator/Calculator';
+import Home from './pages/home/Home';
 import Todo from './pages/todo/Todo';
+import Navbar from './layout/Navbar';
+import Weather from './pages/weather/Weather';
+import PasswordVal from './pages/password/PasswordVal';
+import Calculator from './pages/calculator/Calculator';
 
+
+// App Component
 export default function App() {
   return (
     <BrowserRouter>
+
       <Navbar />
+
       <Routes>
+
         <Route path="/" element={<Navigate to='/home' />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/password" element={<PasswordVal />} />
-        <Route path='/weather' element={<Weather />} />
+
         <Route path='/bmi' element={<Bmi />} />
-        <Route path='/calculator' element={<Calculator />} />
+        <Route path="/home" element={<Home />} />
         <Route path='/todo' element={<Todo />} />
+        <Route path='/weather' element={<Weather />} />
+        <Route path="/password" element={<PasswordVal />} />
+        <Route path='/calculator' element={<Calculator />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
